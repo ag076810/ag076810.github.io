513d9e9dc10310c93a6cbc0dcafbf27e181befb0
ag076810 <88880369+ag076810@users.noreply.github.com>
2022-01-15 00:05:14 +0800

    Add files via upload

--- js/connectToWallet.js
+++ js/connectToWallet.js
@@ -124,12 +124,12 @@ async function connectToContract() {
     if (networkId === 10001) {
         document.getElementById("network").innerHTML = "Test BCH net";
         web3 = new Web3("http://35.220.203.194:8545/");
-        contractAddress = "0xd718CaF10B1D84D82f9C52e58eb2cDF5e13DC27E";
+        contractAddress = "0xf0bF9d19C0d15e00eD17427CdD91a79797C69D01";
         hrefBscscan = "https://www.smartscan.cash/"
     } else if (networkId === 10001) {
         document.getElementById("network").innerHTML = "Main BCH net";
         web3 = new Web3("http://35.220.203.194:8545/");
-        contractAddress = "0xd718CaF10B1D84D82f9C52e58eb2cDF5e13DC27E";
+        contractAddress = "0xf0bF9d19C0d15e00eD17427CdD91a79797C69D01";
         hrefBscscan = "https://www.smartscan.cash/"
         //hrefBscscan=https://bscscan.com
     } else {
